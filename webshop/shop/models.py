3a0b7b0570fd9a8b223fd5c0078663839b0a27ee
Yellow Duck <mvch@triangle.software>
2025-09-30 00:06:19 +0000

    products views

--- webshop/shop/models.py
+++ webshop/shop/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
+
+    def get_absolute_url(self):
+        return reverse("shop:product_list_by_category", args=[self.slug])
 
     class Meta:
         ordering = ['name']
@@ -11,7 +15,7 @@ class Category(models.Model):
         ]
         verbose_name = 'category';
         verbose_name_plural = 'categories';
-
+    
     def __str__(self):
         return self.name
 
@@ -32,6 +36,9 @@ class Product(models.Model):
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse("shop:product_detail", args=[self.id, self.slug])
 
     class Meta:
         ordering = ['name']
