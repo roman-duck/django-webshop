6c291c95c3ea1e9b0c67136e6ed3bc64bbf84b3b
Yellow Duck <mvch@triangle.software>
2025-10-01 16:31:44 +0000

    async task

--- webshop/orders/views.py
+++ webshop/orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .models import OrderItem
+from .tasks import order_created
 
 
 def order_create(request):
@@ -16,7 +17,10 @@ def order_create(request):
                                          price=item['price'],
                                          quantity=item['quantity'])
             cart.clear()
+            # launch asynchronous task
+            order_created.delay(order.id)
             return render(request, 'orders/order/created.html', {'order': order})
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
